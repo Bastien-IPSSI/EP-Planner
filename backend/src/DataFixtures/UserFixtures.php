9131d133f4baa08ed0a850dd7e6523c37eee8f67
Pierre Guitard <pierreguitard78@gmail.com>
2025-03-06 14:57:43 +0100

    new seeds with ouvriers

--- backend/src/DataFixtures/UserFixtures.php
+++ backend/src/DataFixtures/UserFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Employe;
+use App\Entity\Specialite;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -24,11 +26,15 @@ class UserFixtures extends Fixture
               ->setMail('admin@exemple.com')
               ->setRole('ROLE_ADMIN');
         
-
         $hashedPassword = $this->passwordHasher->hashPassword($admin, 'azerty');
         $admin->setPassword($hashedPassword);
 
         $manager->persist($admin);
+
+
+        $specialiteMaconnerie = $manager
+        ->getRepository(Specialite::class)
+        ->findOneBy(['nom' => 'Maçonnerie']);
 
         $user1 = new User();
         $user1->setNom('Dupont')
@@ -38,8 +44,22 @@ class UserFixtures extends Fixture
 
         $hashedPassword1 = $this->passwordHasher->hashPassword($user1, 'azerty');
         $user1->setPassword($hashedPassword1);
-
+        
+        $employe1 = new Employe();
+        $employe1->setDispo(true);
+        $employe1->setSkills(['Cablage électrique', 'Sécurité électrique' ]);
+        $employe1->setSpecialite($specialiteMaconnerie);
+        
+        $employe1->setUser($user1);
+        $user1->setEmploye($employe1);
+        
         $manager->persist($user1);
+        $manager->persist($employe1);
+
+
+        $specialitePlomberie = $manager
+        ->getRepository(Specialite::class)
+        ->findOneBy(['nom' => 'Plomberie']);
 
         $user2 = new User();
         $user2->setNom('Martin')
@@ -50,7 +70,17 @@ class UserFixtures extends Fixture
         $hashedPassword2 = $this->passwordHasher->hashPassword($user2, 'azerty');
         $user2->setPassword($hashedPassword2);
 
+        $employe2 = new Employe();
+        $employe2->setDispo(true);
+        $employe2->setSkills(['Coffrage béton', 'Pose de briques']);
+        $employe2->setSpecialite($specialitePlomberie);
+
+        
+        $employe2->setUser($user2);
+        $user2->setEmploye($employe2);
+
         $manager->persist($user2);
+        $manager->persist($employe2);
 
         $manager->flush();
     }
